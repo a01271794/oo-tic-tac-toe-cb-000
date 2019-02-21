d785253c3e176faeb88ff2f4b3938dd6fb8483b3
Oscar Frederick Perez Reyes <a01271794@itesm.mx>
2019-02-21 06:31:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -84,4 +84,13 @@ class TicTacToe
   def full?
     @board.all? {|index| index == "X" || index = "O"}
   end
+
+  def draw?
+    if !won? && full?
+      return true
+    else
+      return false
+    end
+  end
+  
 end
