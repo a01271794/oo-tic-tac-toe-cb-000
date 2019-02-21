fdb8d3d1c937ca8479f1da3ffc457864d6ec53cf
Oscar Frederick Perez Reyes <a01271794@itesm.mx>
2019-02-21 06:13:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,5 +52,9 @@ class TicTacToe
 
   def turn
     user_input = gets.chomp    
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      player_token = current_player
+    end
   end
 end
